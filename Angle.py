afa9a9f91eba9f3b32d15fa01a4bea0918aa43ba
Vanmors <ivanmorikov48@gmail.com>
2022-02-24 22:09:41 +0300

    .

--- Angle.py
+++ Angle.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import math
 
 gpslat1 = 35  # Долгота начальной точки
@@ -14,20 +13,3 @@ try:
     print('%2.2f' % (degree))
 except:
     print("Division by zero")
-=======
-import math
-
-gpslat1 = 35  # Долгота начальной точки
-gpslong1 = 37  # Широта начальной точки
-
-gpslat2 = 46  # Долгота конечной точки
-gpslong2 = 42  # Широта конечной точки
-
-x = math.fabs(gpslat2 - gpslat1)  # Вычисление x для вектора движения
-y = math.fabs(gpslong2 - gpslong1)  # Вычисление y для вектора движения
-try:
-    degree = math.degrees(math.asin((y / (math.sqrt(x * x + y * y)))))  # Вычисление градуса поворота
-    print('%2.2f' % (degree))
-except:
-    print("Division by zero")
->>>>>>> origin/master
