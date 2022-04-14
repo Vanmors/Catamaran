f1787d1319352df10aeb664440072bb33dde3843
Vanmors <ivanmorikov48@gmail.com>
2022-04-14 17:01:54 +0300

    Чтение GPS данных с файла

--- FileReader.py
+++ FileReader.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 f = open('D:\script.txt', 'r')  # чтение данных с файла
+
 first_string = f.readline()  # чтение первой строки
 second_string = f.readline()  # чтение второй строки
+
 split1 = first_string.split(':')  # разделение через двоеточие первой строки
 split2 = second_string.split(':')  # разделение через двоеточие второй строки
+
 longitude = float(split1[1])  # долгота
 latitude = float(split2[1])  # широта
-# print(longitude)
+#   print(longitude)
 # print(latitude)
